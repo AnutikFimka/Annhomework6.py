8aa30e741b46d81b2a1af222edf677bcbd04e07c
AnutikFimka <anutik200289@mail.ru>
2024-07-26 17:30:25 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+my_dict={'Anapa': 43, 'Soshi': 35, 'Gagra': 39}
+print(my_dict)
+print(my_dict["Soshi"])
+my_dict["Tuapse"]=36
+print(my_dict)
+my_dict.update({'Moskva': 28,
+                'Piter': 27})
+print(my_dict)
+my_dict.pop('Gagra')
+print(my_dict)
+my_set={1,2,3,4,5,'Bannan','Anapa',5,4,3,2,1,}
+print(my_set)
+print(my_set.add('Tula'))
+print(my_set.add('Daha'))
+print(my_set)
+print(my_set.discard('Bannan'))
+print(my_set)
